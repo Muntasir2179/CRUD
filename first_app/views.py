b9730a180c7dfbf3f37d0955f61e50a37f3e8891
Muntaisr Ahmed <muntasirahmed2179@gmail.com>
2022-12-07 17:48:11 +0600

    form-data-save-added

--- first_app/views.py
+++ first_app/views.py
@@ -13,6 +13,18 @@ def index(request):
 
 def student_form(request):
     form = forms.StudentForm()
+
+    # (request.method == "POST") when user clicks on the submit button
+    if request.method == "POST":
+        form = forms.StudentForm(request.POST)
+
+        if form.is_valid():
+            # (commit = True) pushes the forms date into the database
+            form.save(commit=True)
+            return index(request)
+            # calling index() function to return into the index page
+
+    # sending data to the HTML page so that we can access the data and show that data into the web page
     diction = {
         'title': "Student Form",
         'student_form': form,
